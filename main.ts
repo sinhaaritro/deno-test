06579cb605ceb1e668e51d5312b6f868b130df26
sinhaaritro <sinhaaritro@gmail.com>
2025-03-13 09:05:51 +0530

    Initial DevContainerSetup

---
+++ main.ts
@@ -0,0 +1,69 @@
+import { Hono } from "@hono/hono";
+
+const app = new Hono();
+const kv = await Deno.openKv();
+
+interface Tree {
+  id: string;
+  species: string;
+  age: number;
+  location: string;
+}
+
+const oak: Tree = {
+  id: "3",
+  species: "oak",
+  age: 3,
+  location: "Jim's Park",
+};
+
+await kv.set(["trees", oak.id], oak);
+const newTree = await kv.get(["trees", oak.id]);
+console.log(newTree);
+console.log(newTree.key);
+console.log(newTree.value);
+await kv.set(["trees", oak.id], oak);
+
+app.post("/trees", async (c) => {
+  const { id, species, age, location } = await c.req.json();
+  const tree: Tree = { id, species, age, location };
+  await kv.set(["trees", id], tree);
+  return c.json({
+    message: `We just added a ${species} tree!`,
+  });
+});
+
+app.get("/trees/:id", async (c) => {
+  const id = c.req.param("id");
+  const tree = await kv.get(["trees", id]);
+  if (!tree.value) {
+    return c.json({ message: "Tree not found" }, 404);
+  }
+  return c.json(tree.value);
+});
+
+app.delete("/trees/:id", async (c) => {
+  const id = c.req.param("id");
+  await kv.delete(["trees", id]);
+  return c.json({
+    message: `Tree ${id} has been cut down!`,
+  });
+});
+
+app.put("/trees/:id", async (c) => {
+  const id = c.req.param("id");
+  const { species, age, location } = await c.req.json();
+  const updatedTree: Tree = { id, species, age, location };
+  await kv.set(["trees", id], updatedTree);
+  return c.json({
+    message: `Tree has relocated to ${location}!`,
+  });
+});
+
+app.get("/", async (c) => {
+  return c.json({
+    message: `Hi this is working`,
+  });
+});
+
+Deno.serve({ port: 8000, hostname: "0.0.0.0" }, app.fetch);
